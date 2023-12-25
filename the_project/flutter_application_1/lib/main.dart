7d4835ed67873224e5255a0e98d7a158dd7c8f26
ahmed mohamed refaat <ahta601@gmail.com>
2023-12-25 22:06:14 +0200

    splach screen modified

--- the_project/flutter_application_1/lib/main.dart
+++ the_project/flutter_application_1/lib/main.dart
@@ -1,37 +1,8 @@
-// void main() async {
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(MyApp());
-//}
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   ); // or any other service initialization
-//   runApp(const MyApp());
-// }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
-//     return MaterialApp(
-//       initialRoute: 'welcom',
-//       debugShowCheckedModeBanner: false,
-//       routes: {
-//         'welcom': (context) => LoginScreen(),
-//         'home': (context) => const home(),
-//       },
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-import 'package:project1/components/theme.dart';
-import 'package:project1/firebase_options.dart';
-import 'package:project1/splash/splash_screen.dart';
+import 'components/theme.dart';
+import 'firebase_options.dart';
+import 'splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
@@ -53,6 +24,6 @@ class MyApp extends StatelessWidget {
         theme: lightMode,
         darkTheme: darkMode,
         themeMode: ThemeMode.light,
-        home: const SplashScreen());
+        home: SplashScreen());
   }
 }
--- the_project/flutter_application_1/lib/splash/splash_screen.dart
+++ the_project/flutter_application_1/lib/splash/splash_screen.dart
@@ -1,28 +1,73 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
+
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:project1/screens/after_splash.dart';
+import '../screens/after_splash.dart';
 
-class SplashScreen extends StatelessWidget {
-  static const String routeName = "splash";
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
 
-  const SplashScreen({Key? key}) : super(key: key);
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    );
+    _animationController.forward();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const AfterSplash()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Container(
-        height: 600,
-        width: 400,
-        child: Center(
-          child: Image.asset(
-            "assets/images/dilevery_logo.png",
-            fit: BoxFit.cover,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/dilevery_logo.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'WASALLY',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
         ),
       ),
-      backgroundColor: Colors.white,
-      splashTransition: SplashTransition.rotationTransition,
-      nextScreen: const AfterSplash(),
     );
   }
 }
